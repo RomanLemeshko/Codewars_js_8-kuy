1584469e90b72f186dba83c14efbf488908e0144
Roman Lemeshko <146091287+RomanLemeshko@users.noreply.github.com>
2023-11-07 20:25:54 +0300

    Update ConvertNumberToReversedArrayOfDigits.js

--- ConvertNumberToReversedArrayOfDigits.js
+++ ConvertNumberToReversedArrayOfDigits.js
@@ -1,8 +1,3 @@
-// Convert number to reversed array of digits
-// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
-
-// Необходимо вернуть цифры этого числа в массиве в обратном порядке.
-
 // Example(Input => Output):
 // 35231 => [1,3,2,5,3]
 // 0 => [0]
